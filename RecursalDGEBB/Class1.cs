e745b0c03f1b3b4508fba6b2e7dafb8e75a534d9
Anilton Vigânigo Jr <viganigojr@gmail.com>
2022-03-04 16:59:12 -0300

    Refatoração do Código da Lib DGE BB

--- RecursalDGEBB/Class1.cs
+++ RecursalDGEBB/Class1.cs
@@ -82,20 +82,41 @@ namespace Recursal_DGE
                 var saldoDataBase = "";
                 var validaLinha = false;
                 string line;
+                var firstValue = true;
 
                 if (File.Exists(ArquivoTXT_DGEBB))
                 {
                     using (StreamReader reader = new StreamReader(ArquivoTXT_DGEBB))
                     {
-                        File.WriteAllText(ArquivoCSV_DGEBB, "contaJudicial;parcela;processo;reclamante;reclamada;saldoCapital;saldoDataBase\r\n", System.Text.Encoding.UTF8);
+                        File.WriteAllText(ArquivoCSV_DGEBB, "contaJudicial;parcela;processo;reclamante;saldoCapital;saldoDataBase\r\n", System.Text.Encoding.UTF8);
                         while ((line = reader.ReadLine()) != null)
                         {
+                            //Extrai os valores dos Saldos: Capital e Data Base;
+                            var regexGeral = ("\\s([0-9]+[\\,])?([0-9]+[\\.,])+([0-9]{2})+");
+                            MatchCollection matches = Regex.Matches(line, regexGeral);
+                            foreach (Match balanceValue in matches)
+                            {
+                                if (matches.Count == 2 & firstValue == true)
+                                {
+                                    saldoCapital = balanceValue.Value;
+                                    firstValue = false;
+                                    line = line.Replace(balanceValue.Value, "");
+                                }
+                                else
+                                {
+                                    saldoDataBase = balanceValue.Value;
+                                    firstValue = true;
+                                    line = line.Replace(balanceValue.Value, "");
+                                }
+                            }
+
                             if (line.Contains("Conv"))
                             {
                                 var split = line.Split('-');
                                 reclamada = split[0].Trim();
                                 split = reclamada.Split(' ');
-                                reclamada = split[0] + " " + split[1];
+                                //reclamada = split[0] + " " + split[1];
+                                reclamada = split[0];
                             }
 
                             if (line.Contains("Trabalhista"))
@@ -109,12 +130,18 @@ namespace Recursal_DGE
                                 Match match = Regex.Match(line, regexCtaProcessual, RegexOptions.ECMAScript);
                                 if (match.Success)
                                 {
-                                    var splitLine = line.Split(' ');
+                                    var splitLine = line.Replace("  "," ").Split(' ');
                                     ctaJudicial = splitLine[1];
                                     parcela = splitLine[2];
                                     processNumber = splitLine[3];
 
-                                    line = line.Replace(ctaJudicial, "").Replace(parcela, "").Replace(processNumber, "").Replace(reclamada, "").Trim();
+                                    line = line.Replace(ctaJudicial, "").Replace(parcela, "").Replace(processNumber, "").Replace("-", "").Replace(".","").TrimStart();
+
+                                    //Formatação do Número do Processo
+                                    if (processNumber.Length == 20 && !processNumber.Contains("-") && !processNumber.Contains(".") && !processNumber.Contains("/"))
+                                    {
+                                        processNumber = processNumber.Insert(7,"-").Insert(10,".").Insert(15,".").Insert(17,".").Insert(20,".");
+                                    }
 
                                     //Verifica se a line contém dígitos no início
                                     var regexCleanLineA = ("^\\d+");
@@ -123,52 +150,30 @@ namespace Recursal_DGE
                                     {
                                         line = line.Replace(match.Value, "");
                                     }
-                                }
+                                }                                                               
 
-                                //Bloco que extrai as informações: Saldos, Reclamada e Reclamante
-                                var regexGeral = ("\\s([0-9]+[\\,])?([0-9]+[\\.,])+([0-9]{2})+");
-                                match = Regex.Match(line, regexGeral, RegexOptions.ECMAScript);
+                                //Extrai as informações da Reclamada;
+                                var regexReclamada = (reclamada + "(([^;]+)?)\\s");
+                                match = Regex.Match(line, regexReclamada, RegexOptions.ECMAScript);
                                 if (match.Success)
                                 {
-                                    saldoCapital = match.Value;
-                                    line = line.Replace(saldoCapital, "");
-                                    match = Regex.Match(line, regexGeral, RegexOptions.ECMAScript);
-                                    if (!match.Success)
+                                    reclamante = line.Replace(match.Value,"").Trim();
+                                    if (reclamante != "")
                                     {
-                                        saldoDataBase = saldoCapital;
-                                        reclamante = line.Replace(reclamada, "").Trim();
-
-                                        var regexCleanLineB = ("[^a-z ]\\ *([.0-9])*\\d");
-                                        match = Regex.Match(reclamante, regexCleanLineB, RegexOptions.ECMAScript);
-                                        if (match.Success)
-                                        {
-                                            reclamante = reclamante.Replace(match.Value, "").Trim();
-                                        }
-                                    }
-                                    else
-                                    {
-                                        match = Regex.Match(line, regexGeral, RegexOptions.ECMAScript);
-                                        saldoDataBase = match.Value;
-                                        reclamante = line.Replace(match.Value, "").Replace(reclamada, "").Trim();
-
-                                        var regexCleanLineB = ("[^a-z ]\\ *([.0-9])*\\d");
-                                        match = Regex.Match(reclamante, regexCleanLineB, RegexOptions.ECMAScript);
-                                        if (match.Success)
-                                        {
-                                            reclamante = reclamante.Replace(match.Value, "").Trim();
-                                        }
-                                    }
-                                    File.AppendAllText(ArquivoCSV_DGEBB, ctaJudicial.ToString() + ";"
-                                        + parcela.ToString() + ";" + processNumber.ToString() + ";"
-                                        + reclamante.ToString() + ";" + reclamada.ToString() + ";"
-                                        + saldoCapital.ToString() + ";"
+                                        File.AppendAllText(ArquivoCSV_DGEBB, "'" + ctaJudicial.ToString() + "'" + ";"
+                                        + "'" + parcela.ToString() + "'" + ";" + "'" + processNumber.ToString() + "'" + ";"
+                                        + reclamante.ToString() + ";" + saldoCapital.ToString() + ";"
                                         + saldoDataBase.ToString() + ";\r\n", System.Text.Encoding.UTF8);
+                                    }
+                                    
                                 }
-
+                                
                             }
+
                         }
                     }
                 }
+
 
             }
             catch (Exception e)
--- RecursalDGEBB/Program.cs
+++ RecursalDGEBB/Program.cs
@@ -11,8 +11,8 @@ namespace RecursalDGEBB
         static void Main(string[] args)
         {
             var Recursal_DGEBB = new Recursal_DGE.DGE_BB_Class();
-            Recursal_DGEBB.Execute(@"C:\Users\vigan\OneDrive\Área de Trabalho\Projetos ONE4\VML Recursal - Protótipo\NEW DGE BB\Arquivos Teste\SALDO_DGE BB_15.09.2021.txt",
-                @"C:\Users\vigan\OneDrive\Área de Trabalho\Projetos ONE4\VML Recursal - Protótipo\NEW DGE BB\Arquivos Teste\SALDO_DGE BB_15.09.2021.csv");
+            Recursal_DGEBB.Execute(@"C:\Users\vigan\OneDrive\Área de Trabalho\Projetos ONE4\VML Recursal - Protótipo\NEW DGE BB\Arquivos Teste\Banco BTG\SALDO_DGE BB_14.09.2021.txt",
+                @"C:\Users\vigan\OneDrive\Área de Trabalho\Projetos ONE4\VML Recursal - Protótipo\NEW DGE BB\Arquivos Teste\Banco BTG\SALDO_DGE BB_14.09.2021.csv");
             Console.WriteLine("Fim");
         }
     }
